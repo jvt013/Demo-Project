085cda23ff7859a74557b70cc91f884dcf7581a5
yelitzam <ymendez290@gmail.com>
2020-11-23 18:53:06 -0800

    Initial Commit

---
+++ rootify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rootify
+//
+//  Created by Litza Mendez on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
