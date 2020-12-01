4c6021ced2f4420106fd8fe6cd707ab5a34ae875
Fariha Mohamed <alifariha242@gmail.com>
2020-11-30 18:16:19 -0800

    Added Plant Navigation Controller

---
+++ rootify/CameraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraViewController.swift
+//  rootify
+//
+//  Created by Fariha Mohamed on 11/29/20.
+//
+
+import UIKit
+
+class CameraViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ rootify/PlantViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlantViewController.swift
+//  rootify
+//
+//  Created by Fariha Mohamed on 11/29/20.
+//
+
+import UIKit
+
+class PlantViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func scanButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
