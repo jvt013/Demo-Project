9e6d11765df1d2383e467c3c0f2581a4759466d2
Jaehoon Han <JaehoonHan1@gmail.com>
2020-12-04 18:43:21 -0500

    add custom plant vc

---
+++ rootify/CustomPlantViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CustomPlantViewController.swift
+//  rootify
+//
+//  Created by Jae Hoon Han on 12/4/20.
+//
+
+import UIKit
+
+class CustomPlantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
