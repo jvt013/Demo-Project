f2a14bfebf7878dec108adc3bf8ff9695b39fbc4
Fariha Mohamed <alifariha242@gmail.com>
2020-12-11 12:44:01 -0800

    User can open camera or photogallery

---
+++ rootify/CameraViewController.swift
@@ -0,0 +1,77 @@
+//
+//  CameraViewController.swift
+//  rootify
+//
+//  Created by Fariha Mohamed on 12/11/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
+                           UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+ //Open Camera Button
+    
+    @IBAction func openCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+//Open Gallery Button
+    @IBAction func openGalleryButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            picker.sourceType = .photoLibrary
+        } else {
+            picker.sourceType = .camera        }
+        
+        present(picker, animated: true, completion: nil)
+        
+//Resize Image and insert into imageView    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+        
+    
+        
+        
+        
+    /*    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+ }
+}
