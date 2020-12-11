8c7e0ab7007b53843bd91d994fce581290c7db6b
Jenny Trinh <jvt013@ucsd.edu>
2020-12-11 02:54:59 -0800

    Added design/background elements, prototype reminders

---
+++ rootify/reminderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  reminderTableViewCell.swift
+//  rootify
+//
+//  Created by Jennifer Trinh on 12/11/20.
+//
+
+import UIKit
+
+class reminderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
