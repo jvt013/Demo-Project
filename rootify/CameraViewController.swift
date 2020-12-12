3bf1b3b071c119d97d8ece89b20c2c06f742cbad
Fariha Mohamed <alifariha242@gmail.com>
2020-12-12 01:14:13 -0800

    Connected application to PlantId Api

--- rootify/CameraViewController.swift
+++ rootify/CameraViewController.swift
@@ -18,8 +18,37 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
 
         // Do any additional setup after loading the view.
     }
-    
- //Open Camera Button
+  //Scan button
+    @IBAction func scanButton(_ sender: Any) {
+      //let url = URL(string:"https://api.plant.id/v2/usage_info/?api_key=Ok64FUBCOOV95IkTngjl7hxLZkVxwH7nuj4iCvPc2zBZo5GbwY&language=en-US&page=1")!
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.plant.id/v2/identify")! as URL)
+        request.addValue("Ok64FUBCOOV95IkTngjl7hxLZkVxwH7nuj4iCvPc2zBZo5GbwY", forHTTPHeaderField: "X-Auth-Token")
+        request.httpMethod = "GET" // or POST or whatever
+       // let task = URLSession.dataTaskWithRequest(<#URLRequest#>){(data, response, error) in // request anything needed during session
+        //NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: OperationQueue.main) { (response, data, error) in
+            // handle your data here
+            print("Success")
+        }
+        //let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+      /*  let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            // This will run when the network request returns
+               if let error = error {
+                  print(error.localizedDescription)
+               } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                  print(dataDictionary)
+                  // TODO: Get the array of movies
+                  // TODO: Store the movies in a property to use elsewhere
+                  // TODO: Reload your table view data
+
+               }
+            }
+         task.resume() //uncomment later
+*/
+    }
+
+    //Open Camera Button
     
     @IBAction func openCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
