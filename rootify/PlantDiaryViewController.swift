9e6c0d8996dacf5fef113d2b165c2ad3dc6ba345
Jaehoon Han <JaehoonHan1@gmail.com>
2020-12-12 04:12:37 -0500

    Revert "Added prototype cell and hard coded plant names to plant diary VC"
    This reverts commit ea313e9361b66342917188780c8fea6c7aae7119.

--- rootify/PlantDiaryViewController.swift
+++ rootify/PlantDiaryViewController.swift
@@ -7,33 +7,14 @@
 
 import UIKit
 
-class PlantDiaryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    
-    
-    @IBOutlet weak var plantDiaryTableView: UITableView!
-    @IBOutlet weak var plantName: UILabel!
-    @IBOutlet weak var plantPhoto: UIImageView!
-    
-    let plantNameArray = ["Dracaena trifasciata", "Pothos", "Aglaonema", "Peace Lily"]
+class PlantDiaryViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        plantDiaryTableView.reloadData()
-    }
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return plantNameArray.count
-   }
-   
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
-    cell.textLabel?.text = plantNameArray[indexPath.item]
-       return cell
-   }
+    
 
     /*
     // MARK: - Navigation
